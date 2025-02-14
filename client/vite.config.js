5f1abeb1a464ee76d88aadee48fac92ea16b680e
PinakGupta <pinakgupta99@gmail.com>
2025-02-14 20:23:54 +0530

    final commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy:{
       '/api':{
         target: 'https://estate-ease-gray.vercel.app',
-        secure: false,
+        secure:false,
       },
     },
   },
